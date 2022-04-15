e1b0ac79a36141a5247abc322cfc1f020517896e
Rajan <53229081+Rajansinghbaliyan@users.noreply.github.com>
2022-04-15 17:23:19 +0530

    added the update controller

---
+++ src/main/kotlin/cherry/technologies/restbasic/utils/CustomExceptions/NotFoundException.kt
@@ -0,0 +1,5 @@
+package cherry.technologies.restbasic.utils.CustomExceptions
+
+import java.lang.RuntimeException
+
+class NotFoundException(p0: String?) : RuntimeException(p0)
--- src/main/kotlin/cherry/technologies/restbasic/web/services/employee/EmployeeServiceImpl.kt
+++ src/main/kotlin/cherry/technologies/restbasic/web/services/employee/EmployeeServiceImpl.kt
@@ -1,6 +1,7 @@
 package cherry.technologies.restbasic.web.services.employee
 
 import cherry.technologies.restbasic.domain.Employee
+import cherry.technologies.restbasic.utils.CustomExceptions.NotFoundException
 import cherry.technologies.restbasic.web.repositories.employee.EmployeeRepository
 import org.springframework.stereotype.Service
 import java.util.*
@@ -13,7 +14,10 @@ class EmployeeServiceImpl(val employeeRepository: EmployeeRepository):EmployeeSe
 
     override fun save(employee: Employee): Employee = employeeRepository.save(employee.copy(id = null))
 
-    override fun update(employee: Employee): Employee = employeeRepository.save(employee)
+    override fun update(employee: Employee): Employee = with(employee) {
+        val empFromDb = id?.let { getById(it) } ?: throw NotFoundException("Please provide the correct id.")
+        employeeRepository.save(employee.copy(id = empFromDb.id))
+    }
 
     override fun delete(id: UUID) = employeeRepository.deleteById(id)
 }
